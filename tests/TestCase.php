77f19d57edb41215f512aa3741339610cc168aee
Arthur Reis <hartielag@gmail.com>
2026-01-15 09:06:35 -0300

    fix(tests): fake inertia rendering to prevent vite issues

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,15 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Inertia\Inertia;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Mock do Inertia para os testes
+        Inertia::fake();
+    }
 }
